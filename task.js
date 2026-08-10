24d20b212aca7b5f19d1a412c600222fd48f83d6
unknown <akmkamrulhasanreyad@gmail.com>
2026-08-10 10:08:13 +0600

    completed assignment problem 01

--- task.js
+++ task.js
@@ -1,1 +1,17 @@
-console.log('task.js');
+// Problem 1 — Student Introduction Generator
+const studentIntroduction = (student) => {
+    if (typeof student !== 'object' || Array.isArray(student)) {
+        return "Invalid"
+    }
+
+    const keys = Object.keys(student);
+    if (!keys.includes('name') ||
+        !keys.includes('age') ||
+        !keys.includes('course')
+    ) {
+        return "Invalid"
+    }
+
+    const { name, age, course } = student;
+    return `My name is ${name}. I am ${age} years old. I am learning ${course}.`
+}
